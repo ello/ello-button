85e8530840e23a8aa794e2463a65862bc77855b4
Justin-Holmes <justin.ryan.holmes@icloud.com>
2016-01-28 11:49:25 -0700

    Add pry to Gemfile and added a route for button

--- server.rb
+++ server.rb
@@ -2,4 +2,11 @@ class HelloWorldApp < Sinatra::Base
   get '/' do
     "Hello, world!"
   end
+
+  get '/button' do
+    @color    = params[:color]
+    @username = params[:username]
+
+    erb :button
+  end
 end
