b0ecc48bea93bb3e687678661652532c43d9830e
Justin-Holmes <justin.ryan.holmes@icloud.com>
2016-02-01 13:50:35 -0700

    Add redirect test

--- test/features/button_test.rb
+++ test/features/button_test.rb
@@ -20,4 +20,14 @@ class ButtonTest < Minitest::Test
     assert page.has_css? '.white'
     refute page.has_css? '.black'
   end
+
+  def test_it_redirects_to_profile_on_click
+    visit '/button?color=black&username=ello'
+    find('a').click
+
+    # Capybara is strict with redirects compared to browsers, so this will allow the test from breaking
+    rescue Capybara::InfiniteRedirectError
+
+    assert_equal page.current_url, 'https://ello.co/ello'
+  end
 end
